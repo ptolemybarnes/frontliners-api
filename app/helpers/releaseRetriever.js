ff3365691e662b976b278073191d33ae966b5770
Ptolemy <ptolemybarnes@gmail.com>
2015-02-21 19:15:16 +0000

    Write function to get release_id from Rise JSON object.

--- app/helpers/releaseRetriever.js
+++ app/helpers/releaseRetriever.js
@@ -1,5 +1,5 @@
 var request = require('request');
-var fs      = 
+var _       = require('underscore');
 
 ReleaseRetriever = function() {
   this.board_id = 8299;
@@ -18,6 +18,7 @@ ReleaseRetriever.prototype.lastRelease = function(callback) {
 
 ReleaseRetriever.prototype.processRelease = function(releaseJSON) {
   var players = releaseJSON.players;
+  var releaseID = this.getReleaseID(releaseJSON).toString();
   var output  = [];
   for(var i = 0; i < players.length; i ++) {
     var player = players[i];
@@ -25,10 +26,15 @@ ReleaseRetriever.prototype.processRelease = function(releaseJSON) {
       username: player.player_identifier,
       full_name: player.full_name,
       profile_picture_url: player.profile_picture_url,
-      rank: i + 1 }
+      rank: player.releases[releaseID].rank }
     output.push(playerHash);
   }
   return output;
+}
+
+ReleaseRetriever.prototype.getReleaseID = function(Json) {
+  var releaseNums =  _.keys(Json.board.releases)
+  return _.max(releaseNums);
 }
 
 module.exports = ReleaseRetriever;
--- spec/releaseRetrieverSpec.js
+++ spec/releaseRetrieverSpec.js
@@ -14,12 +14,17 @@ describe('ReleaseRetriever', function() {
       { username: 'ozoesono',
         full_name: 'Ozo',
         profile_picture_url: "https://pbs.twimg.com/profile_images/422441705833369602/gRrKy7D3_normal.png",
-        rank: 1 },
+        rank: '1' },
       { username: 'guacamolay',
         full_name: 'Ptolemy',
         profile_picture_url: 'https://pbs.twimg.com/profile_images/1743558449/jf28_normal.jpg',
-        rank: 2 }
+        rank: '2' }
       ]);
   })
-  
+ 
+  it('retrieves the release id from the JSON', function() {
+    rt = new releaseRetriever;
+    var exampleJSON = JSON.parse(fs.readFileSync('./spec/example_release.txt').toString());
+    expect(rt.getReleaseID(exampleJSON)).toEqual('2330718');
+  });
 })

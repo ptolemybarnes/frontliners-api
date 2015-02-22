a2c47087d08881fafb0014f1d6e08a22beb67028
Ptolemy <ptolemybarnes@gmail.com>
2015-02-22 10:46:10 +0000

    Remove unnecessary call to getLastRelease function.

--- app/server.js
+++ app/server.js
@@ -1,15 +1,8 @@
 var restify          = require('restify');
 var releaseRetriever = require('./helpers/releaseRetriever')
-var twit             = require('twit');
 var port             = process.env.PORT || 8080;
 
-var releaseRetriever = new releaseRetriever();
-var twit             = new twit({
-  consumer_key: process.env.FL_TWIT_KEY, 
-  consumer_secret: process.env.FL_TWIT_SECRET,
-  access_token: process.env.FL_TWIT_TOKEN,
-  access_token_secret: process.env.FL_TOKEN_SECRET
-});
+
 
 var server = restify.createServer({
   name: 'frontliners-api'
@@ -21,13 +14,11 @@ server.get('/', function create(req, res, next) {
 });
 
 server.get('/scoreboard', function create(req, res, next) {
-  releaseRetriever.lastRelease(function(err, req, body) {
-
-    releaseRetriever.getCurrentRelease(function(data) {
-      console.log(data);
-        res.send(data);
-    });
+  releaseRetriever.getCurrentRelease(function(data) {
+    console.log(data);
+      res.send(data);
   });
+
   return next();
 });
 
